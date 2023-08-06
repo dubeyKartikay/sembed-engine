1f7c335ccd91ad47f62d4b3d5fd1db782132cb93
kartikay dubey <kartikayd@Nobara.sys>
2023-08-06 14:28:23 +0530

    debugging Datasets file

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,15 +2,18 @@
 #include "load_from_binary.hpp"
 #include <filesystem>
 #include <string>
+#include "dataset.hpp"
 namespace fs = std::filesystem;
 int main(int argc, char ** argv){
     // std::cout << argc << argv[0] << '\n';
     fs::path data = argv[1];
-
     std::cout << fs::current_path() / data << '\n';
     fs::path dataFilePath = fs::current_path() / data;
-    std::vector<std::vector<float>*> *  vec_data =  load_from_binary(dataFilePath);
-    for (int i = 0; i < 301; i++)
+   /*  std::vector<std::vector<float>*> *  vec_data =  load_from_binary(dataFilePath); */
+    InMemoryDataSet dataset (dataFilePath);
+    std::vector<std::vector<float>*> *  vec_data = dataset.getNVectorsFromIndex(0, dataset.getN());
+  
+  for (int i = 0; i < dataset.getDimentions(); i++)
     {
         std::cout << vec_data->at(0)->at(i) << " ";
     }
